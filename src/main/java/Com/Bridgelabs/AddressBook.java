dd24e0a6da137bc932d2a914d422ab9621b57c0b
unknown <gownideepu23@gmail.com>
2023-03-11 11:42:27 +0530

    added

--- src/main/java/Com/Bridgelabs/AddressBook.java
+++ src/main/java/Com/Bridgelabs/AddressBook.java
@@ -1,35 +1,59 @@
 package Com.Bridgelabs;
 
-import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
-
+import java.io.*;
+import java.nio.Buffer;
+import java.util.*;
 public class AddressBook {
-    static Scanner scanner = new Scanner(System.in);
-    List<Contact> Contactlist = new ArrayList<>();
+    static Scanner src = new Scanner(System.in);
+    public static Map<String, ArrayList<Contact>> addressBook = new HashMap<>();
 
-    void AddressBook(Contact contact) {
-        Contactlist.add(contact);
+    public void AddressBookContact(String addressBookName, Contact contact) {
+        ArrayList<Contact> addressBookDetails = addressBook.getOrDefault(addressBookName, new ArrayList<Contact>());
+        addressBookDetails.add(contact);
+        addressBook.put(addressBookName, addressBookDetails);
     }
 
-    void addContact() {
-        System.out.println("Enter name");
-        String name = scanner.next();
-        System.out.println("Enter surname");
-        String surname = scanner.next();
-        System.out.println("Enter address");
-        String address = scanner.next();
-        System.out.println("Enter city");
-        String city = scanner.next();
-        System.out.println("Enter state");
-        String state = scanner.next();
-        System.out.println("Enter email");
-        String email = scanner.next();
-        System.out.println("Enter contactnumber");
-        String contactnumber = scanner.next();
-
-        Contact contact = new Contact(name,surname,address,city,state,email,contactnumber);
-        AddressBook(contact);
+    public void showContact(){
+        for (Map.Entry<String, ArrayList<Contact>> ent: addressBook.entrySet()){
+            String key=ent.getKey();
+            ArrayList<Contact> val=ent.getValue();
+            System.out.println(key + " \n");
+            for (Contact  cont: val ){
+                System.out.println(cont.getName());
+                System.out.println(cont.getSurname());
+                System.out.println(cont.getAddress());
+                System.out.println(cont.getContactNumber());
+                System.out.println(cont.getCity());
+                System.out.println(cont.getState());
+                System.out.println(cont.getGmail());
+            }
+        }
     }
+    public void fileWriter() {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\eclips workspace\\WorkShop\\src\\main\\java\\Com\\Bridgelabs\\AddressBook"));
+            for (Map.Entry<String, ArrayList<Contact>> ent: addressBook.entrySet()) {
+                String key = ent.getKey();
+                ArrayList<Contact> val = ent.getValue();
+                writer.write(key );
+                writer.newLine();
+                for (Contact  cont: val ){
+                    writer.write(cont.getName()+ " ");
+                    writer.write(cont.getSurname()+ " ");
+                    writer.write(cont.getContactNumber()+ " ");
+                    writer.write(cont.getAddress()+ " ");
+                    writer.write(cont.getCity()+ " ");
+                    writer.write(cont.getState()+ " ");
+                    writer.write(cont.getGmail()+ " ");
+                    writer.write(" \n");
+                }
+            }
+            writer.flush();
+            writer.close();
+        }catch (IOException e){
+            System.out.println(e);
+
+        }
+    }
+
 }
-
--- src/main/java/Com/Bridgelabs/AddressBookMain.java
+++ src/main/java/Com/Bridgelabs/AddressBookMain.java
@@ -2,8 +2,13 @@ package Com.Bridgelabs;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-        AddressBook Ram=new AddressBook();
-        Ram.addContact();
+        AddressBook addressbook=new AddressBook();
+        addressbook.AddressBookContact("Family",new Contact("Deepthi","Gowni","Madanapalle","Mpl","Aps","98772332","g@gmail.com"));
+        addressbook.AddressBookContact("Family",new Contact("Priya","G","Madanapalle","Mp","Ap","987324272332","p@gmail.com"));
+        addressbook.AddressBookContact("Friend",new Contact("Ammu","G","Madanapalle","M","A","9873242","aaM@gmamil.com"));
+
+        addressbook.showContact();
+        addressbook.fileWriter();
 
     }
 }
